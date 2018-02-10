350bb089d8dc869d6551a45badb8d97541278ccd
meriy100 <ttattataa@gmail.com>
2018-02-10 14:23:29 +0900

    init

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,22 +1,27 @@
 module.exports = {
   devtool: 'inline-source-map',
   entry: {
-    application: './src/application.js'
+    application: './src/application.ts'
   },
   output: {
     path: './build',
     filename: '[name].bundle.js'
   },
-  node: {
-    fs: 'empty'
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
   },
   module: {
-    loaders: [
+    rules: [
       {
+        test: /\.(ts|tsx)?$/,
+        use: [
+          {loader: 'ts-loader'}
+        ]
+      }, {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: 'babel?presets[]=react,presets[]=es2015,presets[]=stage-2'
-      }, {
+      } , {
         test: /\.json$/,
         loader: 'json'
       }, {
