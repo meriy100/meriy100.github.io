1f0a86f5e31ca87fa4a0bc2b16e09c01c8928269
meriy100 <ttattataa@gmail.com>
2018-02-11 11:32:23 +0900

    タイトルいい感じ

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,40 +1,67 @@
 import * as React from 'react'
+import * as FreeStyle from 'free-style'
+
 
 import * as Variables from '../Variables'
 
-var headerStyle = {
-  color: Variables.whiteColor,
+var style = FreeStyle.create();
+
+var headerStyle = style.registerStyle({
+  color: Variables.pureWhiteColor,
   backgroundImage: 'url("imgs/homeHeader.jpg")',
   width: "100%",
   height: "620px",
   WebkitTransition: 'all', // note the capital 'W' here
   msTransition: 'all', // 'ms' is the only lowercase vendor prefix
   backgroundSize: "cover",
-  grass: {
-    backgroundColor: Variables.blackColor,
-    opacity: 0.6,  
-    width: "100%",
-    height: "100%",
-  }
-};
+  position: 'relative',
+  '&:before' : {
+      content: `''`,
+      background: 'rgba(44,62,79,0.5)',
+      position: 'absolute',
+      height : '620px',
+      width : '100%',
+  } 
+});
 
+var titleStyle = style.registerStyle({
+  position: 'relative',
+  textAlign: 'center',
+  paddingTop:  '160px',
+  'h1' : {
+    fontSize: '3.2rem',
+  },
+  'p' : {
+    fontSize: '2rem',
+  }
+})
+
+var styleElement = document.createElement('style')
+styleElement.textContent = style.getStyles()
+document.head.appendChild(styleElement)
 
 const Header = () => (
-  <div style={headerStyle}> 
-    <div style={headerStyle.grass}>
+  <div className={headerStyle}> 
+    <div className={titleStyle}>
       <h1>Kouta Kariyado</h1>
       <p>meriy100</p>
-      <h2>About</h2>
-      <p>大学4年からエンジニアとして活動</p>
-      <p>主に Web 開発をやっています</p>
-      <p>学部の研究は Rails の国際化改善</p>
-      <p>修士ではソースコードの問題箇所検出を行いました</p>
-      <p>2018年度より新社会人となります.  よろしくおねがいします</p>
-      <h2>Contact</h2>
-      <p>○ ttattataa@gmail.com</p>
-      <p>○ https://github.com/meriy100</p>
-      <p>○ https://qiita.com/meriy100</p>
-      <p>○ https://www.facebook.com/kouta.kariyadon</p>
+    </div>
+    <div className='row'>
+      <div className='col-md-8 '>
+        <h2>About</h2>
+        <p>大学4年からエンジニアとして活動</p>
+        <p>主に Web 開発をやっています</p>
+        <p>学部の研究は Rails の国際化改善</p>
+        <p>修士ではソースコードの問題箇所検出を行いました</p>
+        <p>2018年度より新社会人となります.  よろしくおねがいします</p>
+      </div>
+      <div className='col-md-4'>
+        <h2>Contact</h2>
+        <p>○ ttattataa@gmail.com</p>
+        <p>○ https://github.com/meriy100</p>
+        <p>○ https://qiita.com/meriy100</p>
+        <p>○ https://www.facebook.com/kouta.kariyadon</p>
+      </div>
     </div>
   </div>
 )
